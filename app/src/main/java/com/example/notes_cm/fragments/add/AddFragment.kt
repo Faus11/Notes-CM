2332960a1e33e58a83836b86ce6c99702f7ea0bd
Faus11 <148981356+Faus11@users.noreply.github.com>
2024-03-30 13:46:22 +0000

    ADICIONEI STRINGS, LANDSCAPE, CORES NOS BOTÕES - LAB2 FEITO

---
+++ app/src/main/java/com/example/notes_cm/fragments/add/AddFragment.kt
@@ -0,0 +1,60 @@
+package com.example.notes_cm.fragments.add
+
+import android.icu.util.Calendar
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.notes_cm.R
+import com.example.notes_cm.data.entities.DateConverter
+import com.example.notes_cm.data.entities.Note
+import com.example.notes_cm.data.vm.NoteViewModel
+
+class AddFragment : Fragment() {
+    private lateinit var mNoteViewModel: NoteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        ViewModelProvider(this)[NoteViewModel::class.java].also { this.mNoteViewModel = it }
+
+        val button = view.findViewById<Button>(R.id.save)
+        button.setOnClickListener {
+            addNote()
+        }
+
+        val backButton = view.findViewById<Button>(R.id.backToList)
+        backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
+
+        return view
+    }
+
+    private fun addNote() {
+        val noteText = view?.findViewById<EditText>(R.id.addNote)?.text.toString()
+        val currentDate =  Calendar.getInstance().time
+        val date = DateConverter.fromDate(currentDate)
+
+        if(noteText.isEmpty() || noteText.length < 5) {
+            Toast.makeText(view?.context, R.string.length_warning, Toast.LENGTH_LONG).show()
+        }
+        else {
+            val note = Note(0, noteText, date)
+
+            mNoteViewModel.addNote(note)
+
+            Toast.makeText(requireContext(), R.string.success_message, Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
+    }
+}
